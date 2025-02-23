5206d69d48a1feab587e978264b1ca3ea9f71463
Mausam Rajbanshi <rajbanshimausam@gmail.com>
2025-02-23 23:24:01 +0545

    initializing fast api application

---
+++ app/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def home():
+    return
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
