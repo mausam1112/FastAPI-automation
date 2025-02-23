acdd60db0534dbab49768fad452f5ee90727e563
Mausam Rajbanshi <rajbanshimausam@gmail.com>
2025-02-23 23:00:03 +0545

    rendering index template from fastapi app

--- app/main.py
+++ app/main.py
@@ -11,9 +11,9 @@ app.mount("/static", StaticFiles(directory="../static"), name="static")
 templates = Jinja2Templates(directory="../templates")
 
 
-@app.get("/")
-def home():
-    return
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 
 if __name__ == "__main__":
